440fb01aa6c7a469d66c5357dc235032af75458e
taivn07 <taivn07@gmail.com>
2013-10-07 05:43:59 +0000

    edit config file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,2 +1,20 @@
 # coding: utf-8
-Admin.create! email: 'admin@ic-com.co.jp', password: 'test1234' if Admin.count == 0
+Admin.create! email: 'test@gmail.com', password: 'test1234' if Admin.count == 0
+
+unless Rails.env.production?
+    connection = ActiveRecord::Base.connection
+    connection.tables.each do |table|
+        connection.execute("TRUNCATE #{table}") unless table == "schema_migrations"
+    end
+
+    sql = File.read('db/data.sql')
+    statements = sql.split(/;$/)
+    statements.pop
+
+    ActiveRecord::Base.transaction do
+        statements.each do |statement|
+            connection.execute(statement)
+        end
+    end
+end
+
