4f993683aa2b6e06b08cfda2441d2ab42c22a348
Mai The Tai <tai.mt@baristride.co.jp>
2013-10-22 18:28:13 +0900

    Add fog config

---
+++ config/initializers/fog.rb
@@ -0,0 +1,8 @@
+CarrierWave.configure do |config|
+  config.fog_credentials = {
+    :provider               => 'AWS',
+    :aws_access_key_id      => Setting.aws.access_key_id,
+    :aws_secret_access_key  => Setting.aws.secret_access_key
+  }
+  config.fog_directory  = Setting.s3.bucket
+end
